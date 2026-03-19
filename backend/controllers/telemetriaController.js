7ae36c288d52026e0376007813584da53d92d040
Nicolas <nikkaloo45@gmail.com>
2026-03-19 21:27:04 +0100

    Implementazione Webhook per notifiche allarmi

--- backend/controllers/telemetriaController.js
+++ backend/controllers/telemetriaController.js
@@ -44,6 +44,30 @@ exports.postTelemetria = async (req, res) => {
             console.log(`Attenzione, allarme per il veicolo: ${targa}`);
         }
 
+        //Implementazione Webhook tramite per inviare una notifica ai manager tramite API di telegram
+        try {
+            const botToken = process.env.TELEGRAM_BOT_TOKEN;
+            const chatId = process.env.TELEGRAM_CHAT_ID;
+            const telegramUrl = 'https://api.telegram.org/bot' + botToken + '/sendMessage'
+            //Messaggio da mandare:
+            const messaggioTelegram = '!!ALLARME Drivedesk!! Intervieni al più presto! \nVeicolo:' + targa + '\nCausa:' + guasto;
+            //Utilizzo fetch per inviare la chiamata POST al server di telegram
+            await fetch(telegramUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    chat_id: chatId,
+                    text: messaggioTelegram
+                })
+            });
+            console.log('Messaggio inviato a Telegram per il veicolo ${targa} con causa ${guasto}');
+        } catch (error) {
+            console.error('Errore durante invio del messaggio a Telegram:', error);
+        }
+
+
         //Salvo i dati aggiornati
         await veicolo.save();
         res.json({msg: 'Dati ricevuti', statoVeicolo: veicolo.stato});
